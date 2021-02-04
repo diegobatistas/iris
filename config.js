b8c8491d3f6edaf67bb8d29a3413d3e6806b2006
diegobatistas <78523792+diegobatistas@users.noreply.github.com>
2021-02-04 05:27:18 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2292,12 +2292,12 @@ module.exports = kconfig = async (kill, message) => {
             if (args.length == 0) return kill.reply(from, 'Sei la, tem algo errado nisso ai!', id)
             const gplk = body.slice(6)
             const tGr = await kill.getAllGroups()
-            const minMem = 30 // PRECISA TER ISSO DE MEMBRO PRA ENTRAR
+            const minMem = 01 // PRECISA TER ISSO DE MEMBRO PRA ENTRAR
             const isLink = gplk.match(/(https:\/\/chat.whatsapp.com)/gi)
             const check = await kill.inviteInfo(gplk)
             if (!isLink) return kill.reply(from, 'Link errado', id)
             if (tGr.length > 6) return kill.reply(from, 'Já estou no maximo de grupos, desculpe.', id)
-            if (check.size < minMem) return kill.reply(from, 'Só posso funcionar em grupos com mais de 30 pessoas.', id)
+            if (check.size < minMem) return kill.reply(from, 'Só posso funcionar em grupos com mais de 01 pessoas.', id)
             if (check.status == 200) {
                 await kill.joinGroupViaLink(gplk).then(() => kill.reply(from, 'Entrando no grupo...'))
             } else {
